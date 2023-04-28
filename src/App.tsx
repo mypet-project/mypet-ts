7d16c0fa660fbecfddeb21c1de70112db21515e3
cintdesouza <cintdesouza@gmail.com>
2023-04-28 17:22:46 -0300

    feature: added toasts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import { LoginPage } from "./pages/LoginPage";
 import { GlobalStyle } from "./styles/GlobalStyles";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
     <>
     <GlobalStyle />
+    <ToastContainer />
     <Routes>
       <Route path="/" element={<LoginPage/>} />
     </Routes>
